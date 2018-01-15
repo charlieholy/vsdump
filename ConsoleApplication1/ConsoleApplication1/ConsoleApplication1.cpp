e2d955a15f3de6f3a9ad9ed11f35f2247c90f272
charlie <daad777@126.com>
2018-01-15 20:29:22 +0800

    try move

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,14 @@
 //
 
 #include "stdafx.h"
-
-
+#include <windows.h>
+int aaa = 1;
 int main()
-{
+{	
+	MessageBox(0, NULL, NULL, MB_OK);
+	aaa = 0x889977;
+	MessageBox(0, NULL, NULL, MB_OK);
+	printf("%d", aaa);
     return 0;
 }
 
