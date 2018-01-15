d3594ca01c06088374683c7e2895defaafae758a
charlie <daad777@126.com>
2018-01-15 21:12:31 +0800

    call add

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,15 +4,19 @@
 #include "stdafx.h"
 #include <windows.h>
 int aaa = 1;
+int add(int a, int b) 
+{
+	return a + b;
+}
 int main()
 {	
 	MessageBox(0, NULL, NULL, MB_OK);
 	//aaa = 0x889977;		//mov dword ptr [0x013D3018], 0x889977  .data
 	//__asm mov dword ptr ds:[0x00213018], 0x889977
-	
-	__asm mov aaa, 0x889977;//安全的写法  
+	__asm mov aaa, 0x889977;//安全的写法 
+	aaa = add(0x11, 0x88);
 	MessageBox(0, NULL, NULL, MB_OK);
 	printf("%d", aaa);
-    return 0;
+     return 0;
 }
 
