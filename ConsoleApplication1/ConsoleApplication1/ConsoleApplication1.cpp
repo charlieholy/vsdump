bfc874a5c54ef0b48441915eef5551c170278445
charlie <daad777@126.com>
2018-01-15 20:54:55 +0800

    asm mov

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,7 +7,10 @@ int aaa = 1;
 int main()
 {	
 	MessageBox(0, NULL, NULL, MB_OK);
-	aaa = 0x889977;
+	//aaa = 0x889977;		//mov dword ptr [0x013D3018], 0x889977  .data
+	//__asm mov dword ptr ds:[0x00213018], 0x889977
+	
+	__asm mov aaa, 0x889977;//安全的写法  
 	MessageBox(0, NULL, NULL, MB_OK);
 	printf("%d", aaa);
     return 0;
